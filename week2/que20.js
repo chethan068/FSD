eafb47a28ba7f22bc68547ade05bfeac74a4cda1
Abu999b <srinivasaraghavanguda291004@gmail.com>
2025-01-29 09:21:10 +0530

    WEEK2 Commit 2

---
+++ week2/que20.js
@@ -0,0 +1,45 @@
+class ResettableIterator {
+    constructor(iterable) {
+      this.iterable = iterable;
+      this.index = 0; 
+    }
+ 
+    
+    [Symbol.iterator]() {
+      return this;
+    }
+ 
+    
+    next() {
+  
+      if (this.index >= this.iterable.length) {
+        return { done: true };
+      }
+     
+     
+      const value = this.iterable[this.index];
+      this.index++;
+      return { value, done: false };
+    }
+ 
+ 
+    reset() {
+      this.index = 0;
+    }
+  }
+ 
+
+  const numbers = [1, 2, 3, 4, 5];
+  const resettableIterator = new ResettableIterator(numbers);
+ 
+
+  for (const num of resettableIterator) {
+    console.log(num);
+  }
+ 
+
+  resettableIterator.reset();
+ 
+  for (const num of resettableIterator) {
+    console.log(num);
+  }
---
+++ week2/que22.js
@@ -0,0 +1,24 @@
+function* combinedSequence(arr) {
+    let a = 0, b = 1;  
+    let i = 0;
+ 
+    
+    while (true) {
+      if (i < arr.length) {
+        yield arr[i];  
+      }
+     
+      yield a;  
+      [a, b] = [b, a + b]; 
+    }
+  }
+ 
+
+  const arr = [10, 20, 30, 40, 50];  
+  const seqGen = combinedSequence(arr);
+ 
+
+  for (let i = 0; i < 10; i++) {
+    console.log(seqGen.next().value);
+  }
+ 
